5e2b1111a9ea4af4688636543be9a6ab5c7dbfcb
Essie0715 <56476148+Essie0715@users.noreply.github.com>
2021-02-14 19:34:03 +1100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -13,13 +13,13 @@ import time
 
 
 #Info that you want to collect 
-data_info = {'date':[],'twit':[]} #Tweets time_tag and content
+data_info = {'date':[],'twit':[]} #Tweets time tag and content
 data_url = {"url":[]} #Tweets URL
-company = "LSValue"
+company = "LSValue" #User tag 
 
-driver = webdriver.Firefox(executable_path='C:\Program Files\Mozilla Firefox\geckodriver.exe') 
+driver = webdriver.Firefox(executable_path='C:\Program Files\Mozilla Firefox\geckodriver.exe') #Change path as per required 
 
-
+#Time line 
 year_list = range(2010,2020)
 month_list = range(1,13)
 for year in year_list:
